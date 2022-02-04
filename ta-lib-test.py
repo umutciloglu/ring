44f21f65a4cd73f1609f9c76b8691a3184e3dd14
Umut Ciloglu <umutciloglu06@gmail.com>
2022-02-04 15:57:25 +0300

    fiddled with ta-lib bolinger bands but didn't implemented it yet

--- ta-lib-test.py
+++ ta-lib-test.py
@@ -1,7 +1,8 @@
+# import imp
 import talib,numpy
 from numpy import genfromtxt
 
-data = genfromtxt("../5min.csv", delimiter=",")
+data = genfromtxt("/home/umut/Documents/binance/5min.csv", delimiter=",")
 
 # print(data[0])
 
@@ -9,6 +10,14 @@ close = data[:,4]
 
 rsi = talib.RSI(close)
 
-print(rsi[:250])
+bbands = talib.BBANDS(close, timeperiod=5, nbdevup=2, nbdevdn=2, matype=0)
+
+i = 4
+
+print('upper        middle       lower')
+
+while i <= 50:
+    print("%12f %12f %12f"%(bbands[0][i], bbands[1][i], bbands[2][i]))
+    i = i + 1
 
 # print(close)
