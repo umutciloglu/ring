f573708974eee8dd2fbbcc9e2c4bb3c2ca4997e0
Umut Ciloglu <umutciloglu06@gmail.com>
2022-02-05 13:37:42 +0300

    add script to fetch monthly 15min data for 2019

--- backtrader-test.py
+++ backtrader-test.py
@@ -1,22 +1,28 @@
 import backtrader as bt
 
-class RSIStrategy(bt.Strategy):
+class TestStrategy(bt.Strategy):
     def __init__(self):
         self.RSI = bt.talib.RSI(self.data, period=14)
+        self.bband = bt.indicators.BBands()
+        self.dataclose = self.datas[0].close
 
     def next(self):
         if self.RSI < 30 and not self.position:
-            self.buy(size=0.1)
+            self.buy(size=1)
         if self.RSI > 70 and self.position:
             self.close()
+        # if self.dataclose <= self.bband.lines.mid and not self.position:
+        #     self.buy(size=0.1)
+        # if self.dataclose >= self.bband.lines.top - self.bband.lines.top*0.05 and self.position:
+        #     self.close()
 
 cerebro = bt.Cerebro()
 
-data = bt.feeds.GenericCSVData(dataname='15-min-for-2-months.csv', dtformat=2)
+data = bt.feeds.GenericCSVData(dataname='1mar2019-1apr2019-15min.csv', dtformat=2)
 
 cerebro.adddata(data)
 
-cerebro.addstrategy(RSIStrategy)
+cerebro.addstrategy(TestStrategy)
 cerebro.run()
 
 cerebro.plot()
--- get-data.py
+++ get-data.py
@@ -3,18 +3,36 @@ import config, csv, datetime
 
 client = Client(config.API_KEY, config.API_SECRET)
 
-candles = client.get_historical_klines('BTCUSDT', Client.KLINE_INTERVAL_15MINUTE, "1 jan 2019","1 feb 2019")
+months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+date1 = "1"
+date2 = "1"
+year = 2019
+i = 0
+
+while i < 11:
+    date1 = "1" + months[i] + "2019"
+    date2 = "1" + months[i+1] + "2019"
+    candles = client.get_historical_klines('BTCUSDT', Client.KLINE_INTERVAL_15MINUTE,date1, date2)
+    file = open(date1+"-"+date2+"-15min.csv",'w',newline='')
+    candlestickWriter = csv.writer(file, delimiter=',')
+    for candle in candles:
+        candle[0] = candle[0] / 1000
+        candlestickWriter.writerow(candle)
+    file.close()
+    i = i+1
+
+# candles = client.get_historical_klines('BTCUSDT', Client.KLINE_INTERVAL_15MINUTE, "1 jan 2019","1 feb 2019")
+
+# # for candle in candles:
+# #     timestamp = datetime.datetime.fromtimestamp(candle[0]/1000)
+# #     print(timestamp.strptime())
+
+# file = open('15-min-for-2-months.csv','w',newline='')
+
+# candlestickWriter = csv.writer(file, delimiter=',')
+# # candlestickWriter.writerow(['time','open','high','low','close','volume','close-time','quote-asset-volume'
+# #                             ,'number-of-trades','taker-buy-base-asset-volume','taker-buy-quote-asset-volume','ignore'])
 
 # for candle in candles:
-#     timestamp = datetime.datetime.fromtimestamp(candle[0]/1000)
-#     print(timestamp.strptime())
-
-file = open('15-min-for-2-months.csv','w',newline='')
-
-candlestickWriter = csv.writer(file, delimiter=',')
-# candlestickWriter.writerow(['time','open','high','low','close','volume','close-time','quote-asset-volume'
-#                             ,'number-of-trades','taker-buy-base-asset-volume','taker-buy-quote-asset-volume','ignore'])
-
-for candle in candles:
-    candle[0] = candle[0] / 1000
-    candlestickWriter.writerow(candle)
+#     candle[0] = candle[0] / 1000
+#     candlestickWriter.writerow(candle)
