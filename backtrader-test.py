b634333434b376021a4d94a9b9675ca2054ce00b
Cratoinyan <umutciloglu06@gmail.com>
2022-02-09 14:15:31 +0300

    fiddle with backtrader algorithm

--- backtrader-test.py
+++ backtrader-test.py
@@ -8,6 +8,7 @@ class TestStrategy(bt.Strategy):
         self.dataclose = self.datas[0].close
         self.datahigh = self.datas[0].high
         self.posSize = 0.1
+        self.direction = True
 
     def log(self, txt, dt=None):
         ''' Logging function for this strategy'''
@@ -15,16 +16,21 @@ class TestStrategy(bt.Strategy):
         print('%s, %s' % (dt.isoformat(), txt))
 
     def next(self):
+        if self.dataclose >= self.bbands.lines.bot and not self.direction:
+            self.direction = True
         # if self.RSI < 30 and not self.position:
         #     self.buy(size=1)
         # if self.RSI > 70 and self.position:
         #     self.close()
-        if self.dataclose <= self.bbands.lines.mid and not self.position:
+        if self.dataclose <= self.bbands.lines.mid and not self.position and self.direction:
             self.buy(size=self.posSize)
             # print(self.position.size)
         if self.datahigh >= self.bbands.lines.top and self.position:
             # self.log('Top Band: %.2f Close: %.2f High: %.2f' % (self.bbands.lines.top[0] - self.bbands.lines.top[0] * 0.05, self.dataclose[0],self.datahigh[0]))
             self.close(size=self.posSize)
+        if self.dataclose <= self.bbands.lines.bot:
+            self.close(size = self.posSize)
+            self.direction = False
 
 cerebro = bt.Cerebro()
 
