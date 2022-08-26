fd91f83f899e63e2a9a2a8e226ecb063f99690e0
Cratoinyan <umutciloglu06@gmail.com>
2022-08-26 08:30:42 +0300

    rename the project to ring

--- CLI/setup.py
+++ CLI/setup.py
@@ -2,12 +2,12 @@ from importlib.metadata import entry_points
 from setuptools import setup, find_packages
 
 setup(
-      name = "algo-bot",
+      name = "ring",
       version= "0.0.1",
       packages= find_packages(),
       url="https://github.com/Cratoinyan/algo-trade-bot",
       entry_points = '''
             [console_scripts]
-            algo-bot=algcli:cli
+            ring=algcli:cli
       '''
       )
