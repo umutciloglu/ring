95ef7750e3a2f87300b50108c1fc843b5f985204
Cratoinyan <umutciloglu06@gmail.com>
2022-08-29 11:50:36 +0300

    add help messages to cli commands

--- algcli.py
+++ algcli.py
@@ -8,8 +8,9 @@ def cli():
     pass
 
 @cli.command()
-@click.option("-d/-p", default= False)
+@click.option("-d/-p", default= False, help='enable or disable debug mode(default is disabled)')
 def run(d):
+    """Runs the ring bot on an avaliable port (default is :5000)"""
     os.environ["FLASK_APP"] = "app"
     if d:
         os.environ["FLASK_ENV"] = "development"
@@ -19,6 +20,7 @@ def run(d):
     
 @cli.command()
 def backtest():
+    """Starts the backtesting process using the set strategy in backtrader.py"""
     import backtraderTest
     backtraderTest.run()
     
